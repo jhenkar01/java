f77d022d0acbd25722816bf429b75ad15e1030b2
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:11:31 +0530

    this is my third commit

---
+++ ArrayShort.java
@@ -0,0 +1,12 @@
+public class ArrayShort{
+public static void main(String args[]){
+ short Ben[] = {9999,-12878,9876,987,-12789};
+ System.out.println("lenth is:"+Ben.length);
+ System.out.println("array element in index:"+Ben[3]);
+ System.out.println("array element use of for");
+ for(int i=0;i<Ben.length;i++)
+ {
+    System.out.println("array element:"+Ben[i]);
+ }
+ 
+ }}
