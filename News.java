7b5ace157029aeef27dc8aaa5bbd823c4f68c1b6
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:32:59 +0530

    this is my first commit

---
+++ News.java
@@ -0,0 +1,20 @@
+public class News{
+ public static String newsname = "times of india";
+ public static String newscontent = "jobs for youth";
+ public static float time = 9.30f;
+ public static int endtime = 11;
+ public static boolean usefull = true;
+ public static void main (String[] args){
+ System.out.println("newsname:"+newsname);
+ System.out.println("newscontent:"+newscontent);
+ System.out.println("time:"+time);
+ System.out.println("endtime:"+endtime);
+ System.out.println("usefull:"+usefull);
+  int newsprofit = 100000;
+ String newschanel = "Btv";
+ float timeend = 12.30f;
+ System.out.println("newsprofit:"+newsprofit);
+ System.out.println("newschanel:"+newschanel);
+ System.out.println("timeend:"+timeend);
+ }
+}
