6a868f052bd21a88e66c7c0686ca52c29c7792d4
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:30:22 +0530

    this is my first commit

---
+++ Course.java
@@ -0,0 +1,8 @@
+public class Course{
+public static void main(String args[]){
+int coursecost = 12000;
+float GST = 18/100f;
+float totalcost;
+System.out.println(totalcost = coursecost+(coursecost*GST));
+}
+}
