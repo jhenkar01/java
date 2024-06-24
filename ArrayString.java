653fe7ae4a61cdfc379ec2981690c3564902b35d
unknown <jhenkar.xworkz@gmail.com>
2024-06-24 23:54:46 +0530

    this is my second commit

---
+++ ArrayString.java
@@ -0,0 +1,11 @@
+public class ArrayString{
+public static void main (String[] args){
+ String rainbow[] = {"red","yellow","blue","orange","green","violet","indigo"};
+ System.out.println("array lenth:"+rainbow.length);
+ System.out.println("array in index ex:"+rainbow[3]);
+ System.out.println("array in for");
+ for(int i=0;i<rainbow.length;i++)
+ {
+    System.out.println("array element:"+rainbow[i]);
+ }
+ }}
