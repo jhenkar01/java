7fa39fb06e2d89e2211fe830f0486fbc954cae93
unknown <jhenkar.xworkz@gmail.com>
2024-06-24 12:52:27 +0530

    this is my second commit

---
+++ Addno.java
@@ -0,0 +1,15 @@
+public class Addno {
+ public static void main(String args[]){
+  System.out.println("add two no");
+   int No1 = 12;
+   int No2 = 15;
+   int sum;
+   System.out.println(sum=No1+No2);
+   if(sum==No1)
+   {
+	   System.out.println("yes");
+   }
+   else
+	   System.out.println("no");
+   
+   }}
