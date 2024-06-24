47c8d96316ccc8f547ef9430534aee568ea9f5d3
unknown <jhenkar.xworkz@gmail.com>
2024-06-24 12:31:57 +0530

    this is my first commit

---
+++ Application.java
@@ -0,0 +1,26 @@
+public class Application{
+ public static String Applicationname = "VA";
+ public static String APPcontent = "job";
+ public static float append = 9.30f;
+ public static int lastdate = 11;
+ public static boolean usefull = true;
+ public static String lastday = "monday";
+ public static void main (String[] args){
+ System.out.println("Applicationname:"+Applicationname);
+ System.out.println("APPcontent:"+APPcontent);
+ System.out.println("lastdate :"+lastdate );
+ System.out.println("lastday:"+lastday);
+ System.out.println("usefull:"+usefull);
+  int appno = 100000;
+ String appthrow = "net";
+ float timeend = 12.30f;
+ String appnm = "tch";
+ String appplace = "davngere";
+ System.out.println("appno:"+appno);
+ System.out.println("appthrow:"+appthrow);
+ System.out.println("timeend:"+timeend);
+ System.out.println("appnm:"+appnm);
+ System.out.println(" appplace:"+ appplace);
+ 
+ }
+}
