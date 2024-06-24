c84b6e899fbbea0ff7917f3b64cb2e9bfbb873c7
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:15:55 +0530

    this is my fourth commit

---
+++ ArrayDouble.java
@@ -0,0 +1,12 @@
+public class ArrayDouble{
+public static void main(String args[]){
+ double hsn[] = {99.899,-12.87899,98.767,987,-12.789};
+ System.out.println("lenth is:"+hsn.length);
+ System.out.println("array element in index:"+hsn[3]);
+ System.out.println("array element use of for");
+ for(int i=0;i<hsn.length;i++)
+ {
+    System.out.println("array element:"+hsn[i]);
+ }
+ 
+ }}
