f8e4858a4bf16bb0c7086f2365f70bea4247467f
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:13:05 +0530

    this is my third commit

---
+++ ArrayChar.java
@@ -0,0 +1,11 @@
+public class ArrayChar{
+public static void main (String[] args){
+ char rainbow[] = {'r','y','b','o','g','v','v'};
+ System.out.println("array lenth:"+rainbow.length);
+ System.out.println("array in index ex:"+rainbow[6]);
+ System.out.println("array in for");
+ for(int i=0;i<rainbow.length;i++)
+ {
+    System.out.println("array element:"+rainbow[i]);
+ }
+ }}
