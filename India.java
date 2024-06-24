e0e680f6d77b6e00c409e7e905f413aa988cbad3
unknown <jhenkar.xworkz@gmail.com>
2024-06-24 23:49:22 +0530

    this is my second commit

---
+++ India.java
@@ -0,0 +1,12 @@
+public class India{
+public static void main(String args[]){
+ int india[] = {3,5,6,9,2};
+ System.out.println("lenth is:"+india.length);
+ System.out.println("array element in index:"+india[0]);
+ System.out.println("array element use of for");
+ for(int i=0;i<india.length;i++)
+ {
+    System.out.println("array element:"+india[i]);
+ }
+ 
+ }}
