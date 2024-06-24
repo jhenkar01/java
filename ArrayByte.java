fcfdd6f248b2a097411075792f1f03c26a756537
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:05:04 +0530

    this is my third commit

---
+++ ArrayByte.java
@@ -0,0 +1,12 @@
+public class ArrayByte{
+public static void main(String args[]){
+ byte Home[] = {99,-128,127,0,-12};
+ System.out.println("lenth is:"+Home.length);
+ System.out.println("array element in index:"+Home[3]);
+ System.out.println("array element use of for");
+ for(int i=0;i<Home.length;i++)
+ {
+    System.out.println("array element:"+Home[i]);
+ }
+ 
+ }}
