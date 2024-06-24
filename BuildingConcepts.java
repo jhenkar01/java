eb2b875d487bcda249bbe36c5e56a4253a4e5db6
unknown <jhenkar.xworkz@gmail.com>
2024-06-24 12:34:24 +0530

    this is my first commit

---
+++ BuildingConcepts.java
@@ -0,0 +1,35 @@
+public class BuildingConcepts {
+    public static void main(String[] args) {
+        // Declare variables related to building concepts
+        String buildingName = "Skytower";
+        int floors = 50;
+        double heightInMeters = 275.5;
+        boolean isCommercial = true;
+        char buildingCode = 'A';
+        
+        // Address details
+        String street = "Main Street";
+        String city = "Metropolis";
+        int zipCode = 12345;
+        
+        // Construction details
+        int constructionYear = 2020;
+        double constructionCost = 15000000.50;
+        
+        // Displaying information about the building
+        System.out.println("Building Name: " + buildingName);
+        System.out.println("Number of Floors: " + floors);
+        System.out.println("Height in Meters: " + heightInMeters);
+        System.out.println("Is Commercial: " + isCommercial);
+        System.out.println("Building Code: " + buildingCode);
+        
+        // Displaying address details
+        System.out.println("Street: " + street);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+        
+        // Displaying construction details
+        System.out.println("Construction Year: " + constructionYear);
+        System.out.println("Construction Cost: $" + constructionCost);
+    }
+}
