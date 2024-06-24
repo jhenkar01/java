3d28716585fe0320a50f06b61b6a13275402fef2
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:18:20 +0530

    this is my third commit

---
+++ ArrayFloat.java
@@ -0,0 +1,12 @@
+public class ArrayFloat{
+public static void main(String args[]){
+ float hsn[] = {99.8f,-12.878f,98.76f,98.7f,-12.789f};
+ System.out.println("lenth is:"+hsn.length);
+ System.out.println("array element in index:"+hsn[3]);
+ System.out.println("array element use of for");
+ for(int i=0;i<hsn.length;i++)
+ {
+    System.out.println("array element:"+hsn[i]);
+ }
+ 
+ }}
