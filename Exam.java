b48793d55787f9d855c251ebddcb35885041888a
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:31:41 +0530

    this is my first commit

---
+++ Exam.java
@@ -0,0 +1,12 @@
+public class Exam{
+public static void main(String[] args){
+ int kannad = 99;
+ int english = 83;
+ int hindi = 99;
+ int social = 95;
+ int science = 90;
+ int math = 92;
+ int totalmarks;
+ System.out.print(totalmarks=kannad+english+hindi+social+science+math);
+ }
+ }
