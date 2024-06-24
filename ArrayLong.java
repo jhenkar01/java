c4d58eb451d13aedb9a45e0dfcbc134d64fcf4cc
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 00:22:43 +0530

    this is my third commit

---
+++ ArrayLong.java
@@ -0,0 +1,12 @@
+public class ArrayLong{
+public static void main(String args[]){
+ long bye[] = {99899,-1287899,98767,987,-12789};
+ System.out.println("lenth is:"+bye.length);
+ System.out.println("array element in index:"+bye[4]);
+ System.out.println("array element use of for");
+ for(int i=0;i<bye.length;i++)
+ {
+    System.out.println("array element:"+bye[i]);
+ }
+ 
+ }}
