66ce2f3f36c8244a39363b654553c51f8d875aae
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:25:54 +0530

    this is array 25/06 commit

---
+++ SumOfArray.java
@@ -0,0 +1,12 @@
+public class SumOfArray{
+ public static void main(String args[]){
+   int sumof[] = {12,34,56,77,89};
+   System.out.println("sum of array elements");
+   int sum=0;
+   for(int index=0;index<sumof.length;index++)
+   {
+     System.out.println(+sum = sum + sumof[index]);
+	 
+   }
+   
+   }}
