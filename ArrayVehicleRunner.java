8009eeb807acbb7c06eb9feb378bc95821ac3fa8
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:37:16 +0530

    this is my array 25/06 commit

---
+++ ArrayVehicleRunner.java
@@ -0,0 +1,11 @@
+public class ArrayVehicleRunner{
+public static void main(String args[]){
+   for(int i=0;i<3;i++)
+   {
+     System.out.println("car names:"+ArrayVehicle.car[i]);
+   }
+ 
+ }
+
+
+}
