a6a088a793df7f8e5a5affb5d8e1ef1cb7cc48b2
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:27:14 +0530

    this is array 25/06 commit

---
+++ StaticArray3Runner.java
@@ -0,0 +1,10 @@
+public class StaticArray3Runner{
+	
+	public static void main(String args[]){
+		
+		for(int i=0 ; i<5; i++){
+			System.out.println("The StaticArray1: "+StaticArray3.decimalNumber[i]);
+		}
+	}
+	
+}
