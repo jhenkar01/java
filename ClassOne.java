0dd48c009876a0a22908a555b9cac205bfed541e
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:39:23 +0530

    this is my array 25/06 commit

---
+++ ClassOne.java
@@ -0,0 +1,30 @@
+public class ClassOne{
+	
+	public static void main(String args[]){
+		
+		String names[]={"hassan","Jhenkar","alur","bnglr","karnataka"};
+		
+		int numbers[] ={10,20,30,40,50}; 
+		
+		long phoneNumbers[]={9742616131L,7676356614L,9744256434L,9158324671L,9448728008L};
+		
+		char alphabet[]={'a','b','c','d','e'};
+		
+		
+		for(int index=0; index<=5; index++){
+			
+			System.out.println("The names are: "+names[index]);
+			
+			System.out.println("The numbers are: "+numbers[index]);
+			
+			System.out.println("The Phone numbers are: "+phoneNumbers[index]);
+						
+			System.out.println("The alphabet are: "+alphabet[index]);
+
+			System.out.println("\n");
+
+		}
+		
+		
+	}
+}
