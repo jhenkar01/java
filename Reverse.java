e63a1d5e54b2b520a8eaf742722bdae048c6205d
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:31:28 +0530

    this is my array 25/06 coomit

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse{
+public static void main(String args[]){
+ int reverse_elements[] = {23,76,45,66,11};
+ System.out.println("reverse order");
+ for(int index = 4;index<reverse_elements.length;index--)
+ {
+   System.out.println("reverse elements:"+reverse_elements[index]);
+   }
+ }}
