e572fe02a294606446a0dbe559eaa748d92188d8
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:29:12 +0530

    this is my array 25/06 commit

---
+++ StaticArray2Runner.java
@@ -0,0 +1,10 @@
+public class StaticArray2Runner{
+	
+	public static void main(String args[]){
+		
+		for(int i=0 ; i<5; i++){
+			System.out.println("The StaticArray1: "+StaticArray2.names[i]);
+		}
+	}
+	
+}
