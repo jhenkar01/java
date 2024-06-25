57c52de86cee833cd3ee5c87c975c81224b8fbac
unknown <jhenkar.xworkz@gmail.com>
2024-06-25 23:42:24 +0530

    this is my array 25/06 commit

---
+++ ProjectCost.java
@@ -0,0 +1,10 @@
+public class ProjectCost{
+ static int cost[] = {9000,3000,4500,600};
+  public static void main(String args[]){
+  for(int i=0;i<cost.length;i++)
+  {
+    System.out.println("project cost is:"+cost[i]);
+  }
+  }
+ 
+  }
