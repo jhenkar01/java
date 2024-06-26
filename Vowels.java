159418fe399658f33fedc5b33f4a667168e6afe8
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 23:04:53 +0530

    count vowels

---
+++ Vowels.java
@@ -0,0 +1,12 @@
+//developer needs to count vowels in the array
+public class Vowels{
+ public static void main(String args[]){
+  char vowel[] = {'a','e','i','o','u'};
+  for(int i=0;i<vowel.length;i++)
+  {
+   System.out.println("vowels:"+vowel[i]);
+  }
+   System.out.println(vowel.length);
+ }
+
+}
