03384a095abb11282e51d1903b74b54a514cae14
unknown <jhenkar.xworkz@gmail.com>
2024-06-27 00:08:21 +0530

    data range max

---
+++ MaxDataRange.java
@@ -0,0 +1,17 @@
+ //developer need to display the maximum range of each datatype
+ public class MaxDataRange{
+
+    public static void main(String[] args) {
+        // Displaying maximum range of each primitive data type
+        System.out.println("Primitive Data Type Ranges:");
+   
+        System.out.println("Byte range:  " + Byte.MAX_VALUE  );
+        System.out.println("Short range:  " + Short.MAX_VALUE );
+        System.out.println("Integer range: " + Integer.MAX_VALUE );
+        System.out.println("Long range: " + Long.MAX_VALUE );
+        System.out.println("Float range:  " + Float.MAX_VALUE);
+        System.out.println("Double range: " + Double.MAX_VALUE );
+        System.out.println("Character range: " + (int) Character.MAX_VALUE );
+     
+    }
+}
