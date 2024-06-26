dd01247f28aba92f9a48a6d70ea2c00a0c0e5c94
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 22:55:18 +0530

    display 1 to 100 26/06

---
+++ DisNumb.java
@@ -0,0 +1,11 @@
+//developer need to display the numbers from 1 to 100
+public class DisNumb{
+ public static void main(String args[]){
+ for(int i=1;i<=100;i++)
+ {
+   System.out.println(i);
+ }
+ 
+ }
+}
+
