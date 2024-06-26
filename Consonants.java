70c4dc0550b31dfa0ed149cfc247595c72d443f6
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 23:18:33 +0530

    consonats counts

---
+++ Consonants.java
@@ -0,0 +1,12 @@
+//developer needs to count the consonants in the array
+public class Consonants{
+ public static void main(String args[]){
+  char consonant[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+  for(int i=0;i<consonant.length;i++)
+  {
+   System.out.println("consonant:"+consonant[i]);
+  }
+   System.out.println(consonant.length);
+ }
+
+}
