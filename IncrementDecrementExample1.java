c2b5b1e0edab7c827772b10071baf902724885cb
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 20:25:25 +0530

    post increment decrement 26/06

---
+++ IncrementDecrementExample1.java
@@ -0,0 +1,11 @@
+public class IncrementDecrementExample1 {
+ public static void main(String args[]){
+   int num = 87;
+   System.out.println("the intialize no is"+num);
+   int postincrement = num++;
+   System.out.println("the post increment value is:"+postincrement);
+   int postdecrement = num--;
+   System.out.println("the post decrement value is:"+postdecrement);
+  
+  }
+}
