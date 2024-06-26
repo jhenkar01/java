848678a204fe51c7c3397389ade6501fa301d967
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 23:30:56 +0530

    find cube

---
+++ Cube.java
@@ -0,0 +1,12 @@
+
+//write a program to print the cube of a given number 
+ public class Cube{
+  public static void main(String args[]){
+  int no = 2;
+  int cube = no*no*no;
+  System.out.println("cube of no is:"+cube);
+  
+  }
+  
+  
+  }
