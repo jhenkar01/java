4c11458b0d6648273960cdddefe264b4e2c14380
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 20:28:22 +0530

    post increment decrement

---
+++ PvrIncrementDecrement.java
@@ -0,0 +1,12 @@
+public class PvrIncrementDecrement{
+ public static void main(String args[]){
+ 
+    int NoOfPvr = 87;
+   System.out.println("intialize value =:"+NoOfPvr);
+   int NoOfPvrPostIncrement = NoOfPvr++;
+   System.out.println("increment value =:"+NoOfPvrPostIncrement);
+   int NoOfPvrPostDecrement = NoOfPvr--;
+   System.out.println("decrement value =:"+NoOfPvrPostDecrement);
+   
+ }
+}
---
+++ RatingIncrementDecrement.java
@@ -0,0 +1,12 @@
+public class RatingIncrementDecrement{
+ public static void main(String args[]){
+ 
+    int NoOfRating = 87;
+   System.out.println("intialize value =:"+NoOfRating);
+   int NoOfRatPostIncrement = NoOfRating++;
+   System.out.println("increment value =:"+NoOfRatPostIncrement);
+   int NoOfRatPostDecrement = NoOfRating--;
+   System.out.println("decrement value =:"+NoOfRatPostDecrement);
+   
+ }
+}
