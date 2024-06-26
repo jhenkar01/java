3526b2ffcd2c174ab7f403d24c59727f449c136c
unknown <jhenkar.xworkz@gmail.com>
2024-06-26 21:01:07 +0530

    pre increment decrement 26/06

---
+++ IncrementDecrementFlag.java
@@ -0,0 +1,11 @@
+public class IncrementDecrementFlag {
+ public static void main(String args[]){
+   int num = 87;
+   System.out.println("the intialize no is"+num);
+   int preincrement = ++num;
+   System.out.println("the pre increment value is:"+preincrement);
+   int predecrement = --num;
+   System.out.println("the pre decrement value is:"+predecrement);
+  
+  }
+}
